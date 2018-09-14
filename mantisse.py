846820ada7db9c6ae6bbfa9307c62b0298c3e4fe
Eddy Ikhlef <42712768+m-o-k-a@users.noreply.github.com>
2018-09-14 18:19:28 +0200

    Update mantisse.py

--- mantisse.py
+++ mantisse.py
@@ -9,9 +9,9 @@ result = [] #create list to show all the elements
 result_value = .0 #create a float
 index_value = 0 #start index to -1
 m = input("Ecrivez votre mantisse: ") #get a binary string
-for n in list(str(m))[1:]: #look over the binary string; [1:] to don't count the first digit (always one in the mantisse)
+for n in list(str(m))[1:]: #look over the binary string; [1:] to don't count the first digit (always one in the Significand)
     index_value += 1 #increment index value, to match with the position of the number n
-    if int(n)%2 != 0: #if n is 1
+    if int(n) == 1: #when digit is one, do:
         cache_result = "1/2**"+str(index_value) #result as an element of list
         result_value += 1/(2**int(index_value)) #increment value of the mantisse
         result.append(cache_result) #add the result as an element of list in result list
