8d5516ee7e33f5742d3d17cf5dc8e9b049316aa3
Eddy Ikhlef <42712768+m-o-k-a@users.noreply.github.com>
2018-09-14 18:20:40 +0200

    Update mantisse.py

--- mantisse.py
+++ mantisse.py
@@ -9,10 +9,10 @@ result = [] #create list to show all the elements
 result_value = .0 #create a float
 index_value = 0 #start index to -1
 m = input("Ecrivez votre mantisse: ") #get a binary string
-for n in list(str(m))[1:]: #look over the binary string; [1:] to don't count the first digit (always one in the Significand)
+for n in list(str(m))[1:]: #look over the binary string; [1:] to don't count the first digit (always one in the significand)
     index_value += 1 #increment index value, to match with the position of the number n
     if int(n) == 1: #when digit is one, do:
         cache_result = "1/2**"+str(index_value) #result as an element of list
-        result_value += 1/(2**int(index_value)) #increment value of the mantisse
+        result_value += 1/(2**int(index_value)) #increment value of the significand
         result.append(cache_result) #add the result as an element of list in result list
 print("décomposition de la mantisse:", result, "=", result_value) #print results
