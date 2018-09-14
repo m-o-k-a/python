d15a8efbb8e0ace0359517b34dc465f17e5fe754
Eddy Ikhlef <42712768+m-o-k-a@users.noreply.github.com>
2018-09-14 18:11:45 +0200

    Update mantisse.py

--- mantisse.py
+++ mantisse.py
@@ -1,13 +1,12 @@
 #Mantisse decompose - Eddy Ikhlef
 result = [] #create list to show all the elements
 result_value = .0 #create a float
-index_value = -1 #start index to -1
+index_value = 0 #start index to -1
 m = input("Ecrivez votre mantisse: ") #get a binary string
-
-for n in list(str(m)): #look over the binary string
+for n in list(str(m))[1:]: #look over the binary string; [1:] to don't count the first digit (always one in the mantisse)
     index_value += 1 #increment index value, to match with the position of the number n
     if int(n)%2 != 0: #if n is 1
         cache_result = "1/2**"+str(index_value) #result as an element of list
         result_value += 1/(2**int(index_value)) #increment value of the mantisse
         result.append(cache_result) #add the result as an element of list in result list
-print(result, "=", result_value) #print results
+print("décomposition de la mantisse:", result, "=", result_value) #print results
